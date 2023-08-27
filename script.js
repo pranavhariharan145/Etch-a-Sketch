d093f1f4e8c394be747581d1b71ead2155ff03f5
Pranav <pranavhariharan145@gmail.com>
2023-08-27 10:22:34 +0530

    done with changing colors and eraser

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-
+let size = 16
 let board = document.querySelector(".container-box");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
 
+board.style.gridTemplateColumns = `repeat(16, 1fr)`;
+board.style.gridTemplateRows = `repeat(16, 1fr)`;
 for(let i = 0; i < 256; i++)
 {
     let squares = document.createElement('div')
-    squares.style.backgroundColor = "grey"
     board.insertAdjacentElement("beforeend", squares)
+    squares.addEventListener("mouseover", function(){
+        squares.style.backgroundColor = `${color}`;
+        squares.style.transition = "1s"
+    }
+    )
 }
+function eraser()
+{
+    let erase = document.querySelector(".erase")
+    erase.addEventListener("click", function()
+    {
+        color = "white"
+    })
+}
+function changecol(){
+    let col = document.querySelector(".color");
+    col.addEventListener("input" ,function(c){
+        color = c.target.value;
+    }
+    );
+}
+function all()
+{
+    eraser()
+    changecol()
+
+}
+let color = "black"
+all()
 
