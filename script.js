f88db919df1e54848512e378952cce793692561f
Pranav <pranavhariharan145@gmail.com>
2023-08-27 15:34:49 +0530

    Done with Clear, Write and Erase buttons with functions

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ board.style.gridTemplateRows = `repeat(16, 1fr)`;
 for(let i = 0; i < 256; i++)
 {
     let squares = document.createElement('div')
+    squares.classList.add('smbox');
     board.insertAdjacentElement("beforeend", squares)
     squares.addEventListener("mouseover", function(){
         squares.style.backgroundColor = `${color}`;
-        squares.style.transition = "1s"
+        squares.style.transition = "0.5s"
     }
     )
 }
@@ -25,14 +26,31 @@ function changecol(){
     let col = document.querySelector(".color");
     col.addEventListener("input" ,function(c){
         color = c.target.value;
+        document.querySelector(".color-palette").style.backgroundColor = `${color}`;
     }
     );
+}
+function write()
+{
+    let write = document.querySelector(".write")
+    write.addEventListener("click", function(){
+        color = "black"
+        document.querySelector(".color-palette").style.backgroundColor = "black";
+    })
+}
+function resetboard()
+{
+    const boxes = document.querySelectorAll('.smbox');
+    boxes.forEach(smbox => {
+        smbox.style.backgroundColor = "white";
+    }
+    )
 }
 function all()
 {
     eraser()
     changecol()
-
+    write()
 }
 let color = "black"
 all()
